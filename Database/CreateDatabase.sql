e95861efe42973ff28b56309b2fc31ea114d37be
Maieru <soljoaomaieru@hotmail.com>
2024-07-13 13:33:52 -0300

    Adição de script para criação de database

---
+++ Database/CreateDatabase.sql
@@ -0,0 +1,17 @@
+﻿drop database CursoTeste
+create database CursoTeste
+go
+
+use CursoTeste
+go
+
+create table EncryptionResults (
+	Id int primary key identity(1,1),
+	EncryptedText varchar(max) not null,
+	DecryptedText varchar(max) not null,
+	[Key] varchar(max) not null,
+	Created datetime not null
+)
+go
+
+select * from EncryptionResults
